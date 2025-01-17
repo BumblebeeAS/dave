a2dfddad83c899bdb3dc54417e4f74699a435e3b
SamuelFoo <fooenzesamuel@gmail.com>
2025-01-17 16:21:13 +0800

    Add gz packages install script

---
+++ gz_packages_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install -y libgz-cmake3-dev \
+	libgz-common5-dev \
+	libgz-fuel-tools9-dev \
+	libgz-sim8-dev \
+	libgz-gui8-dev \
+	libgz-launch7-dev \
+	libgz-math7-dev \
+	libgz-msgs10-dev \
+	libgz-physics7-dev \
+	libgz-plugin2-dev \
+	libgz-rendering8-dev \
+	libgz-sensors8-dev \
+	libgz-tools2-dev \
+	libgz-transport13-dev \
+	libgz-utils2-dev \
+	libsdformat14-dev \
+	libsdformat14
